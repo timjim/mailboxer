95c86a779884085e7de06e6a35176ccee0de3eb4
Tim Elder <timothy.j.elder@gmail.com>
2015-12-20 17:12:00 +0000

    Add additional conversational_object argument to send_message method

--- lib/mailboxer/models/messageable.rb
+++ lib/mailboxer/models/messageable.rb
@@ -62,7 +62,7 @@ module Mailboxer
 
       #Sends a messages, starting a new conversation, with the messageable
       #as originator
-      def send_message(recipients, msg_body, subject, sanitize_text=true, attachment=nil, message_timestamp = Time.now)
+      def send_message(recipients, msg_body, subject, sanitize_text=true, attachment=nil, message_timestamp = Time.now, conversationable_object=nil)
         convo = Mailboxer::ConversationBuilder.new({
           :subject    => subject,
           #FROMIGO EDIT
