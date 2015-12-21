a354d79f2ef6c5fe03ff37bd6ca9b482fc7fa673
Tim Elder <timothy.j.elder@gmail.com>
2015-12-21 13:30:33 +0000

    Update arg name

--- lib/mailboxer/models/messageable.rb
+++ lib/mailboxer/models/messageable.rb
@@ -62,7 +62,7 @@ module Mailboxer
 
       #Sends a messages, starting a new conversation, with the messageable
       #as originator
-      def send_message(recipients, msg_body, subject, sanitize_text=true, attachment=nil, message_timestamp = Time.now, booking=nil)
+      def send_message(recipients, msg_body, subject, sanitize_text=true, attachment=nil, message_timestamp = Time.now, booking_id=nil)
         convo = Mailboxer::ConversationBuilder.new({
           :subject    => subject,
           #FROMIGO EDIT
